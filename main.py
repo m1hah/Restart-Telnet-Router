f02eea624fa603bc56c36dec4ea569f0c48550d0
m1hah <70815607+m1hah@users.noreply.github.com>
2022-08-12 01:29:46 +0200

    venv

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from pythonping import ping
+from pyping import pyping
+response_list = ping('1.1.1.1', size=40, count=10)
 import time
 
 print("Hello there!")
 print("Hello there!")
-
+ping('1.1.1.1', verbose=True)
+print(response_list.rtt_avg_ms)
 time.sleep(10)
 
 print("That's it!")
